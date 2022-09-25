c30c0bc7e0a8eb570f5789f062c77a988ad8a872
Maxwell Graves <maxwell.graves@gmal.com>
2022-09-25 02:07:56 -0400

    Tray Bien

--- Practice5/Tray Bien/soln.py
+++ Practice5/Tray Bien/soln.py
@@ -1,47 +1,120 @@
 from math import floor
-import numpy as np
 
 m, n = map(int, input().split())
 
 b = list(map(lambda x: floor(float(x)), input().split()))
 
-bad_spots = {(b[2*x], b[2*x+1]) for x in range(len(b)//2)}
+bad_spots = {(b[2*x+1], b[2*x]) for x in range(len(b)//2)}
 
-grid = np.array([[0 if (i, j) not in bad_spots else 1 for j in range(m)] for i in range(3)])
+top_down = [[0] * m for _ in range(3)]
+bot_up = [[0] * m for _ in range(3)]
+t = [0] * m
+c = [0] * m
 
-def neighbors(i, j):
-    if i > 0:
-        yield i-1, j
-    if i < n-1:
-        yield i+1, j
-    if j > 0:
-        yield i, j-1
-    if j < m-1:
-        yield i, j+1
+# Setup top_down
+for i in range(3):
+    if i == 0:
+        top_down[i][0] = 1
+        continue
+    top_down[i][0] = top_down[i-1][0]
+    if (i, 0) not in bad_spots and (i-1, 0) not in bad_spots:
+        top_down[i][0] += 1 if i < 2 else top_down[i-2][0]
 
-def find_next_zero(grid, i, j):
-    for n_j in range(j, m):
-        for n_i in range(3):
-            if grid[n_i][n_j] == 0:
-                return n_i, n_j
-    return -1, -1
+# Setup bot_up
+for i in reversed(range(3)):
+    if i == 2:
+        bot_up[i][0] = 1
+        continue
+    bot_up[i][0] = bot_up[i+1][0]
+    if (i, 0) not in bad_spots and (i+1, 0) not in bad_spots:
+        bot_up[i][0] += 1 if i != 0 else bot_up[i+2][0]
 
-def backtrack(grid, i, j):
-    # print(grid, end='\n\n')
-    n_i, n_j = find_next_zero(grid, i, j)
-    if n_i == -1 and n_j == -1:
-        return 1
+# Setup T & C
+t[0] = 1
+c[0] = 1
 
-    ans = 0
-    grid[n_i][n_j] = 1
-    for x, y in neighbors(n_i, n_j):
-        if grid[x][y] == 0:
-            grid[x][y] = 1
-            ans += backtrack(grid, n_i, n_j)
-            grid[x][y] = 0
-    ans += backtrack(grid, n_i, n_j)
-    grid[n_i][n_j] = 0
-    return ans
+for col in range(1, m):
+    # Top Down 0
+    top_down[0][col] = top_down[2][col-1]
+    if (0, col) not in bad_spots and (0, col-1) not in bad_spots:
+        top_down[0][col] += bot_up[1][col-1]
+    
+    # Top Down 1
+    top_down[1][col] = top_down[0][col]
+    if (1, col) not in bad_spots:
+        if (0, col) not in bad_spots:
+            top_down[1][col] += top_down[2][col-1]
+        if (1, col-1) not in bad_spots:
+            top_down[1][col] += c[col-1]
+            if (0, col) not in bad_spots and (0, col-1) not in bad_spots:
+                top_down[1][col] += bot_up[2][col-1]
+    
+    # Top Down 2
+    top_down[2][col] = top_down[1][col]
+    if (2, col) not in bad_spots:
+        if (1, col) not in bad_spots:
+            top_down[2][col] += top_down[0][col]
+        if (2, col-1) not in bad_spots:
+            top_down[2][col] += top_down[1][col-1]
+            if (0, col) not in bad_spots and (1, col) not in bad_spots:
+                top_down[2][col] += top_down[1][col-1]
+            if (0, col) not in bad_spots and (0, col-1) not in bad_spots and (1, col) not in bad_spots and (1, col-1) not in bad_spots:
+                top_down[2][col] += top_down[2][col-2] if col > 1 else 1
+            if (0, col) not in bad_spots and (0, col-1) not in bad_spots:
+                top_down[2][col] += t[col-1]
+            if (1, col) not in bad_spots and (1, col-1) not in bad_spots:
+                top_down[2][col] += top_down[0][col-1]
+    
+    # Bottom Up 2
+    bot_up[2][col] = bot_up[0][col-1]
+    if (2, col) not in bad_spots and (2, col-1) not in bad_spots:
+        bot_up[2][col] += top_down[1][col-1]
 
+    # Bottom Up 1
+    bot_up[1][col] = bot_up[2][col]
+    if (1, col) not in bad_spots:
+        if (2, col) not in bad_spots:
+            bot_up[1][col] += bot_up[0][col-1]
+        if (1, col-1) not in bad_spots:
+            bot_up[1][col] += c[col-1]
+            if (2, col) not in bad_spots and (2, col-1) not in bad_spots:
+                bot_up[1][col] += top_down[0][col-1]
+    
+    # Bottom Up 0
+    bot_up[0][col] = bot_up[1][col]
+    if (0, col) not in bad_spots:
+        if (1, col) not in bad_spots:
+            bot_up[0][col] += bot_up[2][col]
+        if (0, col-1) not in bad_spots:
+            bot_up[0][col] += bot_up[1][col-1]
+            if (1, col) not in bad_spots and (2, col) not in bad_spots:
+                bot_up[0][col] += bot_up[1][col-1]
+            if (1, col) not in bad_spots and (1, col-1) not in bad_spots and (2, col) not in bad_spots and (2, col-1) not in bad_spots:
+                bot_up[0][col] += bot_up[0][col-2] if col > 1 else 1
+            if (1, col) not in bad_spots and (1, col-1) not in bad_spots:
+                bot_up[0][col] += bot_up[2][col-1]
+            if (2, col) not in bad_spots and (2, col-1) not in bad_spots:
+                bot_up[0][col] += t[col-1]
+    # T
+    t[col] = top_down[2][col-1]
+    if (1, col) not in bad_spots and (1, col-1) not in bad_spots:
+        t[col] += c[col-1]
 
-print(backtrack(grid, 0, 0))
+    # C
+    c[col] = top_down[2][col-1]
+    if (0, col) not in bad_spots and (0, col-1) not in bad_spots and (2, col) not in bad_spots and (2, col-1) not in bad_spots:
+        c[col] += t[col-1]
+    if (0, col) not in bad_spots and (0, col-1) not in bad_spots:
+        c[col] += bot_up[1][col-1]
+    if (2, col) not in bad_spots and (2, col-1) not in bad_spots:
+        c[col] += top_down[1][col-1]
+
+print(top_down[-1][-1])
+
+# Debug Prints
+# from numpy import array as a
+# print(a(top_down))
+# print(a(bot_up))
+# print(t)
+# print(c)
+# print(bad_spots)
